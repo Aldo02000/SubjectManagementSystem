52fa34eb2ab5a04c66d6e1c75eeae4338ed1b731
Aldo02000 <bega1aldo@gmail.com>
2023-05-15 15:04:15 +0200

    comment app.js

--- SRC/app.js
+++ SRC/app.js
@@ -7,29 +7,41 @@ const flash = require('connect-flash');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 
+// Set the views directory to 'views' and join it with the current directory (__dirname)
 app.set('views', path.join(__dirname, 'views'));
+
+// Set up middleware to parse URL-encoded request bodies
 app.use(express.urlencoded({ extended: false }))
+
+// Set up middleware to display flash messages
 app.use(flash());
+
+// Set up middleware to parse JSON request bodies
 app.use(bodyParser.json());
+
+// Set the static file directory to 'views' and join it with the current directory (__dirname)
 app.use(express.static(path.join(__dirname, 'views')));
+
+// Set the view engine to handlebars (.hbs) and use the 'hbs' engine
 app.engine('hbs', exphbs.engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 
+// Set up session middleware with a secret key, and enable passport authentication and serialization
 app.use(session({
   secret: 'secret-key',
   resave: false,
   saveUninitialized: true
 }));
-
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Initialize passport authentication and attach routes
 const { initPassport } = require('./passportConfig');
 initPassport();
-
 const routes = require('./routes/routes');
 app.use('/', routes);
 
+// Start the server listening on port 3000
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
