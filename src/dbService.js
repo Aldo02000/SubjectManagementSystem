5948a4ac090f17ff0b91e1d52cd49d84326b3dcd
Ylli Bega <yllibega.28@gmail.com>
2023-05-15 14:23:43 +0200

    implementing docker containers for app

--- src/dbService.js
+++ src/dbService.js
@@ -5,10 +5,10 @@ dotenv.config();
 
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 });
 
 const student1Id = 'S00001';
